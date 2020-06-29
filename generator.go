0e09293dae82482e9421c9a1bc7fc22d4b40f9b4
Steven Kaufman <steveplaysguiola@gmail.com>
2020-06-28 23:19:33 -0500

    hopefully fized indentation

--- generator.go
+++ generator.go
@@ -5,87 +5,91 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"html/template"
 	"net/url"
-  "html/template"
-	"os"
 	"time"
 )
 
 // TokenOptions contains the options for the token generated
 type TokenOptions struct {
-  SignedPermissions string
-  SignedStart string
-  SignedExpiry string
-  CanonicalizedResource string
-  SignedIdentifier string
-  SignedIP string
-  SignedProtocol string
-  SignedVersion string
-  SignedResource string
-  SignedSnapshotTime string
-  Rscc string
-  Rscd string
-  Rsce string
-  Rscl string
-  Rsct string
+	SignedPermissions     string
+	SignedStart           string
+	SignedExpiry          string
+	CanonicalizedResource string
+	SignedIdentifier      string
+	SignedIP              string
+	SignedProtocol        string
+	SignedVersion         string
+	SignedResource        string
+	SignedSnapshotTime    string
+	Rscc                  string
+	Rscd                  string
+	Rsce                  string
+	Rscl                  string
+	Rsct                  string
 }
 
 // GenerateToken returns a token using the given resource URI and access key
 func GenerateToken(options *TokenOptions, sasKey string) string {
 
-  stringToSign := options.SignedPermissions + "\n" +
-                  options.SignedStart + "\n" +
-                  options.SignedExpiry + "\n" +
-                  options.CanonicalizedResource + "\n" +
-                  options.SignedIdentifier + "\n" +
-                  options.SignedIP + "\n" +
-                  options.SignedProtocol + "\n" +
-                  options.SignedVersion + "\n" +
-                  options.SignedResource + "\n" +
-                  options.SignedSnapshotTime + "\n" +
-                  options.Rscc + "\n" +
-                  options.Rscd + "\n" +
-                  options.Rsce + "\n" +
-                  options.Rscl + "\n" +
-                  options.Rsct
+	stringToSign := options.SignedPermissions + "\n" +
+		options.SignedStart + "\n" +
+		options.SignedExpiry + "\n" +
+		options.CanonicalizedResource + "\n" +
+		options.SignedIdentifier + "\n" +
+		options.SignedIP + "\n" +
+		options.SignedProtocol + "\n" +
+		options.SignedVersion + "\n" +
+		options.SignedResource + "\n" +
+		options.SignedSnapshotTime + "\n" +
+		options.Rscc + "\n" +
+		options.Rscd + "\n" +
+		options.Rsce + "\n" +
+		options.Rscl + "\n" +
+		options.Rsct
 
-  fmt.Println("String to sign: " + stringToSign)
+	fmt.Println("String to sign: " + stringToSign)
 
 	rawSig := getHmac256(stringToSign, sasKey)
+	fmt.Println("Raw sig: " + rawSig)
+
 	sig := template.URLQueryEscaper(rawSig)
 
 	return fmt.Sprintf("sv=%s&sp=%s&sr=%s&spr=%s&se=%s&sig=%s",
-    options.SignedVersion,
-    options.SignedPermissions,
-    options.SignedResource,
-    options.SignedProtocol,
-    options.SignedExpiry,
-    sig)
+		options.SignedVersion,
+		options.SignedPermissions,
+		options.SignedResource,
+		options.SignedProtocol,
+		options.SignedExpiry,
+		sig)
 }
 
 // GenerateSignedExpiry returns a valid expiration string in x minutes
-func GenerateSignedExpiry (minutes int) string {
+func GenerateSignedExpiry(minutes int) string {
 	// Format expire time
 	expireTime := time.Now().Add(time.Duration(minutes) * time.Minute)
 	// Signed Expiry
-  return expireTime.UTC().Format(time.RFC3339)
+	return expireTime.UTC().Format(time.RFC3339)
 }
 
 // GenerateCanonicalizedResource returns a canonicalized resource string
-func GenerateCanonicalizedResource (uri string) string {
-  u, err := url.Parse(uri)
+func GenerateCanonicalizedResource(uri string, service string) string {
+	u, err := url.Parse(uri)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  return "/blob/" + os.Getenv("SAS_SERVICE") + u.Path
+	return "/blob/" + service + u.Path
 }
 
 func getHmac256(str string, secret string) string {
+	str, err := url.QueryUnescape(str)
+	if err != nil {
+		fmt.Println("Error QueryUnescape-ing string to sign")
+	}
 	key := []byte(secret)
 	h := hmac.New(sha256.New, key)
 	h.Write([]byte(str))
 	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
-
